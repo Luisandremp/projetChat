18dd4fdec87559ddf11e70acca8cd78173c35a60
Joris <blackpopemusical@gmail.com>
2018-04-25 11:35:27 +0200

    IHM text refresh ect

--- projetChat/src/ihm/IHMSwing.java
+++ projetChat/src/ihm/IHMSwing.java
@@ -8,6 +8,7 @@ import java.awt.SystemColor;
 import java.awt.Color;
 import javax.swing.JTextField;
 
+import dao.MessageBean;
 import dao.UserBean;
 
 import javax.swing.JButton;
@@ -18,12 +19,17 @@ import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JEditorPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 public class IHMSwing {
 
 	private JFrame frmChat;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextArea writeMessage;
+	private JTextArea chatArea;
+	private JButton btnSendButton;
 
 	/**
 	 * Launch the application.
@@ -49,16 +55,12 @@ public class IHMSwing {
 	}
 	 
 	//Gestion des Refresh de chaque fenetre du Chat 
-	 
 	
-	 public void updateTextField(boolean visible) {
-	        textField.setText("");
-	    }
-	 
-	  
-	 
-	    public void setVisible(boolean visible) {
-	        frmChat.setVisible(visible);
+	  public void updateChat(ArrayList<MessageBean> liste) {
+		  for (int i = 0; i < liste.size(); i++) {
+			  MessageBean message = liste.get(i);
+			  System.out.println(message);
+	        }
 	    }
 
 	/**
@@ -72,49 +74,37 @@ public class IHMSwing {
 		frmChat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmChat.getContentPane().setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(0, 856, 433, 50);
-		frmChat.getContentPane().add(textField);
-		textField.setColumns(10);
-		
 		JLabel smilieBtn = new JLabel("");
 		smilieBtn.setIcon(new ImageIcon("C:\\Users\\Joris.Dadou\\git\\projetChat\\projetChat\\img\\smile.png"));
-		smilieBtn.setBounds(495, 856, 46, 40);
+		smilieBtn.setBounds(496, 846, 46, 40);
 		frmChat.getContentPane().add(smilieBtn);
 		
 		JLabel whizzBtn = new JLabel("");
 		whizzBtn.setIcon(new ImageIcon("C:\\Users\\Joris.Dadou\\git\\projetChat\\projetChat\\img\\whizz.png"));
-		whizzBtn.setBounds(552, 856, 46, 40);
+		whizzBtn.setBounds(552, 845, 46, 40);
 		frmChat.getContentPane().add(whizzBtn);
 		
 		JLabel sendBtn = new JLabel("");
 		
 		sendBtn.setIcon(new ImageIcon("C:\\Users\\Joris.Dadou\\git\\projetChat\\projetChat\\img\\submit.png"));
-		sendBtn.setBounds(439, 856, 46, 40);
+		sendBtn.setBounds(439, 845, 46, 40);
 		frmChat.getContentPane().add(sendBtn);
 		
 		JLabel photoBtn = new JLabel("");
 		photoBtn.setIcon(new ImageIcon("C:\\Users\\Joris.Dadou\\git\\projetChat\\projetChat\\img\\photo.png"));
-		photoBtn.setBounds(608, 856, 46, 40);
+		photoBtn.setBounds(608, 845, 46, 40);
 		frmChat.getContentPane().add(photoBtn);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(334, 686, 277, 116);
-		frmChat.getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(10, 11, 257, 94);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JButton btnSendButton = new JButton("");
+		btnSendButton = new JButton("");
 		btnSendButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-					System.out.println(textField.getText());
+					System.out.println(writeMessage.getText());
+					chatArea.setText(chatArea.getText() + writeMessage.getText()+"\n");
+					 writeMessage.setText("Tapez votre message...");
+					 
 			}
 		});
-		 btnSendButton.setBounds(439, 856, 46, 40);
+		 btnSendButton.setBounds(439, 845, 46, 40);
 		 btnSendButton.setFocusPainted(false);
 		 btnSendButton.setMargin(new Insets(0, 0, 0, 0));
 		 btnSendButton.setContentAreaFilled(false);
@@ -122,32 +112,57 @@ public class IHMSwing {
 		 btnSendButton. setOpaque(false);
 		frmChat.getContentPane().add(btnSendButton);
 		
-		JButton btnSmileyButton = new JButton("");
-		btnSmileyButton.setBounds(499, 856, 42, 41);
-		 btnSmileyButton.setFocusPainted(false);
-		 btnSmileyButton.setMargin(new Insets(0, 0, 0, 0));
-		 btnSmileyButton.setContentAreaFilled(false);
-		 btnSmileyButton.setBorderPainted(false);
-		 btnSmileyButton. setOpaque(false);
-		frmChat.getContentPane().add(btnSmileyButton);
+		JButton btnSmilieButton = new JButton("");
+		btnSmilieButton.setBounds(496, 845, 42, 41);
+		 btnSmilieButton.setFocusPainted(false);
+		 btnSmilieButton.setMargin(new Insets(0, 0, 0, 0));
+		 btnSmilieButton.setContentAreaFilled(false);
+		 btnSmilieButton.setBorderPainted(false);
+		 btnSmilieButton. setOpaque(false);
+		frmChat.getContentPane().add(btnSmilieButton);
 		
-		JButton btnWizzButton = new JButton("");
-		btnWizzButton.setBounds(551, 856, 47, 40);
-		 btnWizzButton.setFocusPainted(false);
-		 btnWizzButton.setMargin(new Insets(0, 0, 0, 0));
-		 btnWizzButton.setContentAreaFilled(false);
-		 btnWizzButton.setBorderPainted(false);
-		 btnWizzButton. setOpaque(false);
-		frmChat.getContentPane().add(btnWizzButton);
+		JButton btnWhizzButton = new JButton("");
+		btnWhizzButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnWhizzButton.setBounds(552, 845, 47, 40);
+		 btnWhizzButton.setFocusPainted(false);
+		 btnWhizzButton.setMargin(new Insets(0, 0, 0, 0));
+		 btnWhizzButton.setContentAreaFilled(false);
+		 btnWhizzButton.setBorderPainted(false);
+		 btnWhizzButton. setOpaque(false);
+		frmChat.getContentPane().add(btnWhizzButton);
 		
-		JButton btnPictureButton = new JButton("");
-		btnPictureButton.setBounds(604, 856, 50, 40);
-		btnPictureButton.setFocusPainted(false);
-		 btnPictureButton.setMargin(new Insets(0, 0, 0, 0));
-		 btnPictureButton.setContentAreaFilled(false);
-		 btnPictureButton.setBorderPainted(false);
-		 btnPictureButton. setOpaque(false);
-		frmChat.getContentPane().add(btnPictureButton);
+		JButton btnPhotoButton = new JButton("");
+		btnPhotoButton.setBounds(604, 846, 50, 40);
+		btnPhotoButton.setFocusPainted(false);
+		 btnPhotoButton.setMargin(new Insets(0, 0, 0, 0));
+		 btnPhotoButton.setContentAreaFilled(false);
+		 btnPhotoButton.setBorderPainted(false);
+		 btnPhotoButton. setOpaque(false);
+		frmChat.getContentPane().add(btnPhotoButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(20, 846, 406, 40);
+		frmChat.getContentPane().add(scrollPane);
+		
+		writeMessage = new JTextArea();
+		scrollPane.setViewportView(writeMessage);
+		writeMessage.setBackground(Color.YELLOW);
+		writeMessage.setLineWrap(true);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane_1.setBounds(10, 12, 634, 823);
+		frmChat.getContentPane().add(scrollPane_1);
+		
+		chatArea = new JTextArea();
+		chatArea.setLineWrap(true);
+		chatArea.setEditable(false);
+		scrollPane_1.setViewportView(chatArea);
+		
+	
 		
 	}
 }
